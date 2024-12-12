388dbcdf7e3c205e181fdfe3efcc8159ed0f4248
sindi476 <theresiasindiw@gmail.com>
2024-12-12 09:10:15 +0700

    Add files via upload

---
+++ MatematikaCanggih.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Matematika;
+
+/**
+ *
+ * @author USER
+ */
+public class MatematikaCanggih {
+   void modulus(double a, double b, double c){
+        double hasil = a%b%c;
+        System.out.println("Hasil modulus double : " + hasil);
+    }
+    
+    void modulus(int a, int b, int c){
+        int hasil = a%b%c;
+        System.out.println("Hasil modulus int: " + hasil);
+    }
+    
+    void modulus(double a, double b){
+        double hasil = a%b;
+        System.out.println("Hasil modulus double : " + hasil);
+    }
+    
+    void modulus(int a, int b){
+        int hasil = a%b;
+        System.out.println("Hasil modulus int: " + hasil);
+}
+ 
+}
